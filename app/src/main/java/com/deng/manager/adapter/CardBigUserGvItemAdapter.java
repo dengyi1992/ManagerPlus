04e3a7dd96b8283117bf9888e1bf15cb7283aeb1
978548481@qq.com <d7577355>
2016-05-13 13:39:25 +0800

    用户修改，两个切换视图

---
+++ app/src/main/java/com/deng/manager/adapter/CardBigUserGvItemAdapter.java
@@ -0,0 +1,73 @@
+package com.deng.manager.adapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.support.v7.widget.CardView;
+import android.widget.TextView;
+
+import com.deng.manager.R;
+import com.deng.manager.bean.clientUserBean;
+
+public class CardBigUserGvItemAdapter extends BaseAdapter {
+
+    private List<clientUserBean.SuccessBean> objects = new ArrayList<clientUserBean.SuccessBean>();
+
+    private Context context;
+    private LayoutInflater layoutInflater;
+
+    public CardBigUserGvItemAdapter(Context context, List<clientUserBean.SuccessBean> objects) {
+        this.context = context;
+        this.layoutInflater = LayoutInflater.from(context);
+        this.objects = objects;
+    }
+
+    @Override
+    public int getCount() {
+        return objects.size();
+    }
+
+    @Override
+    public clientUserBean.SuccessBean getItem(int position) {
+        return objects.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        if (convertView == null) {
+            convertView = layoutInflater.inflate(R.layout.card_big_user_gv_item, null);
+            convertView.setTag(new ViewHolder(convertView));
+        }
+        initializeViews((clientUserBean.SuccessBean) getItem(position), (ViewHolder) convertView.getTag());
+        return convertView;
+    }
+
+    private void initializeViews(clientUserBean.SuccessBean object, ViewHolder holder) {
+        //TODO implement
+        holder.tvDevid.setText(object.getDevId());
+        holder.tvType.setText(object.getType());
+        holder.tvtime.setText(object.getTime());
+    }
+
+    protected class ViewHolder {
+        private TextView tvDevid;
+        private TextView tvType;
+        private TextView tvtime;
+
+        public ViewHolder(View view) {
+            tvDevid = (TextView) view.findViewById(R.id.tv_devid);
+            tvType = (TextView) view.findViewById(R.id.tv_type);
+            tvtime = (TextView) view.findViewById(R.id.tvtime);
+        }
+    }
+}
--- app/src/main/java/com/deng/manager/fragment/PeopleFragment.java
+++ app/src/main/java/com/deng/manager/fragment/PeopleFragment.java
@@ -8,14 +8,18 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.GridView;
 import android.widget.ListView;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.deng.manager.R;
+import com.deng.manager.adapter.CardBigUserGvItemAdapter;
 import com.deng.manager.adapter.CardBigUserItemAdapter;
 import com.deng.manager.bean.clientUserBean;
 import com.deng.manager.utils.HttpUtils;
+import com.github.clans.fab.FloatingActionButton;
+import com.github.clans.fab.FloatingActionMenu;
 import com.google.gson.Gson;
 
 import java.util.ArrayList;
@@ -30,13 +34,14 @@ public class PeopleFragment extends Fragment {
     private ListView listViewPeople;
     private ProgressBar mProgressBar;
     private ArrayList<clientUserBean.SuccessBean> successBeens;
-    private CardBigUserItemAdapter adapter;
+    private CardBigUserItemAdapter adapterlv;
     Handler handler = new Handler() {
         @Override
         public void handleMessage(Message msg) {
             switch (msg.what) {
                 case SUCCESS:
-                    adapter.notifyDataSetChanged();
+                    adapterlv.notifyDataSetChanged();
+                    adaptergv.notifyDataSetChanged();
                     break;
                 case ERROR:
                     Toast.makeText(getContext(),"网络错误",Toast.LENGTH_SHORT).show();
@@ -46,10 +51,18 @@ public class PeopleFragment extends Fragment {
         }
     };
 
+    private FloatingActionButton mPushMessageFloatingActionButton;
+    private FloatingActionButton mChangeViewFloatingActionButton;
+    private FloatingActionMenu mMessageFloatingActionMenu;
+    private GridView mGvGridView;
+    private CardBigUserGvItemAdapter adaptergv;
+    private boolean islv;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_people, null);
+        islv = true;
         successBeens = new ArrayList<>();
         initView(view);
 
@@ -58,9 +71,42 @@ public class PeopleFragment extends Fragment {
 
     private void initView(View view) {
         listViewPeople = (ListView) view.findViewById(R.id.people_lv);
+        mGvGridView = (GridView) view.findViewById(R.id.people_gv);
         mProgressBar = (ProgressBar) view.findViewById(R.id.progressBar);
-        adapter = new CardBigUserItemAdapter(getContext(), successBeens);
-        listViewPeople.setAdapter(adapter);
+        mPushMessageFloatingActionButton = (FloatingActionButton) view.findViewById(R.id.push_message);
+        mPushMessageFloatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        mChangeViewFloatingActionButton = (FloatingActionButton) view.findViewById(R.id.change_view);
+        mChangeViewFloatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               islv=!islv;
+                if (islv){
+                    listViewPeople.setVisibility(View.VISIBLE);
+                    mGvGridView.setVisibility(View.GONE);
+                }else {
+                    listViewPeople.setVisibility(View.GONE);
+                    mGvGridView.setVisibility(View.VISIBLE);
+                }
+            }
+        });
+        mMessageFloatingActionMenu = (FloatingActionMenu) view.findViewById(R.id.menu_message);
+
+        adapterlv = new CardBigUserItemAdapter(getContext(), successBeens);
+        adaptergv = new CardBigUserGvItemAdapter(getContext(), successBeens);
+        listViewPeople.setAdapter(adapterlv);
+        mGvGridView.setAdapter(adaptergv);
+        if (islv){
+            listViewPeople.setVisibility(View.VISIBLE);
+            mGvGridView.setVisibility(View.GONE);
+        }else {
+            listViewPeople.setVisibility(View.GONE);
+            mGvGridView.setVisibility(View.VISIBLE);
+        }
     }
 
     @Override
